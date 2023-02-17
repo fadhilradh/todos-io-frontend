d4cd09623ec4e0e32505af364f279ba0f5a81f71
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2023-02-17 18:17:47 +0700

    feat: patch todo

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -5,12 +5,15 @@ import { cn } from "../../../utils";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   isLoading?: boolean;
+  wrapperClassName?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, isLoading, ...props }, ref) => {
+  ({ className, wrapperClassName, isLoading, ...props }, ref) => {
     return (
-      <div className="relative flex w-full justify-center">
+      <div
+        className={cn("relative flex w-full justify-center", wrapperClassName)}
+      >
         <input
           className={cn(
             "flex h-10 w-full appearance-none rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-accent-primary focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-100 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900",
